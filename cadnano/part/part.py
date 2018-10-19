34b72f24711b4633b011c715a6caea363c2899c4
Tural Aksel <turalaksel@gmail.com>
2018-10-18 17:09:07 -0700

    Add sequenceOffset to json dictionary for legacy file format

--- cadnano/part/part.py
+++ cadnano/part/part.py
@@ -60,6 +60,9 @@ class Part(CNObject):
         # Selections
         self._selections = {}
 
+        # Sequence offset
+        self._sequenceOffset = 0
+
         if self.__class__ == Part:
             e = "This class is abstract. Perhaps you want HoneycombPart."
             raise NotImplementedError(e)
@@ -108,6 +111,24 @@ class Part(CNObject):
             Document
         """
         return self._document
+    # end def
+
+    def setSequenceOffset(self, offset=0):
+        """Set sequence offset
+
+        Args:
+            offset (int)
+        """
+        self._sequenceOffset = offset
+    # end def
+
+    def getSequenceOffset(self):
+        """Set sequence offset
+
+        Returns:
+            int
+        """
+       return  self._sequenceOffset
     # end def
 
     def setDocument(self, document: DocT):
